0869f02f8c73befc2fd4dfc4f8bd49495bb0a24d
NNCZ12 <57839809+NNCZ12@users.noreply.github.com>
2019-11-24 21:09:32 +0700

    Update stringcontains.go

--- stringcontains.go
+++ stringcontains.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Print("strings.Contains("Hello World", "World")
+	fmt.Print(strings.Contains("Hello World", "world"))
+	fmt.Print(strings.Contains("Hello World", "World"))
 }
